a013602c5544f1e8be3e25267a446b3468a0baca
Ba-Booo <123147457+Ba-Booo@users.noreply.github.com>
2023-04-10 23:12:05 +0900

    Add files via upload

---
+++ 2444_star.py
@@ -0,0 +1,20 @@
+n = input()
+
+
+ls = list(reversed(range(int(n)))) + list(range(1,int(n)))
+
+
+for i in ls:  
+
+    for y in range(i):      #띄어쓰기
+
+        print(" ",end="")
+
+    for x in range((int(n) - i)*2-1):    #글자 출력
+
+        print("*",end="")
+
+
+    print()
+
+    
