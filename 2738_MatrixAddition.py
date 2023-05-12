550cfaf6637dc37cdde031b7dcb68e7fd988293d
Ba-Booo <123147457+Ba-Booo@users.noreply.github.com>
2023-05-12 22:50:37 +0900

    Update 2738_MatrixAddition.py

--- 2738_MatrixAddition.py
+++ 2738_MatrixAddition.py
@@ -5,21 +5,16 @@ list_two = []
 
 for i in range( int( number[1] ) ):                 #list_one 입력
 
-    fake_list = []
-    fake_list.append( input().split(' ') )
-    list_one = list_one + fake_list
+    list_one.append( input().split(' ') )
 
 for i in range( int( number[1] ) ):                 #list_twe 입력
 
-    fake_list = []
-    fake_list.append( input().split(' ') )
-    list_two = list_two + fake_list
-
+    list_two.append( input().split(' ') )
 
 for y in range( int( number[1] ) ):                 #계산 및 출력
 
     for x in range( int( number[0] ) ):
 
-        print( int( list_one[x][y] ) + int( list_two[x][y] ), "",end="")
+        print( int( list_one[y][x] ) + int( list_two[y][x] ), "",end="")
 
     print()
