436e5090de218ca8cf72b3d81689177744cbd37d
Ba-Booo <123147457+Ba-Booo@users.noreply.github.com>
2023-05-16 23:54:59 +0900

    Update 2566_MaximumValue.py

--- 2566_MaximumValue.py
+++ 2566_MaximumValue.py
@@ -3,17 +3,17 @@ ls = []
 
 for y in range(9):      #y좌표
 
-        ls.append( input().split(" ") ) #입력
+    ls.append( input().split(" ") ) #입력
 
-        for x in range(8):      #x좌표
+    for x in range(8):      #x좌표
 
-                if x == 0 and y == 0:   #변수선언용
+        if x == 0 and y == 0:   #변수선언용
 
-                        output = [ls[y][x], y, x]      
+            output = [ls[y][x], y, x]      
 
-                elif int(output[0]) < int(ls[y][x + 1]):        #뒤가 더 클때
+        elif int(output[0]) < int(ls[y][x + 1]):        #뒤가 더 클때
 
-                        output = [ls[y][x + 1], y, x + 1]
+            output = [ls[y][x + 1], y, x + 1]
                         
 
 
