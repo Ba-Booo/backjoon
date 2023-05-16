42e6e6a043371535ad19442bea0a08b3d294ebbd
Ba-Booo <123147457+Ba-Booo@users.noreply.github.com>
2023-05-16 23:51:56 +0900

    Add files via upload

---
+++ 2566_MaximumValue.py
@@ -0,0 +1,21 @@
+ls = []
+
+
+for y in range(9):      #y좌표
+
+        ls.append( input().split(" ") ) #입력
+
+        for x in range(8):      #x좌표
+
+                if x == 0 and y == 0:   #변수선언용
+
+                        output = [ls[y][x], y, x]      
+
+                elif int(output[0]) < int(ls[y][x + 1]):        #뒤가 더 클때
+
+                        output = [ls[y][x + 1], y, x + 1]
+                        
+
+
+print( output[0])                       #출력
+print(output[1] + 1, output[2] + 1 )
