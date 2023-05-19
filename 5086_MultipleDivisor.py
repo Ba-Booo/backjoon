454657f6b9c11bc646fecea24d85a6829bb9a3da
Ba-Booo <123147457+Ba-Booo@users.noreply.github.com>
2023-05-19 23:44:44 +0900

    Update 5086_MultipleDivisor.py

--- 5086_MultipleDivisor.py
+++ 5086_MultipleDivisor.py
@@ -1,21 +1,21 @@
 i = 1
 
-while i != 0:
+while i != 0:       #반복문
     
-    number = input().split(' ')
+    number = input().split(' ')     #입력
 
-    if int( number[0] ) == 0 and int( number[1] ) == 0:
+    if int( number[0] ) == 0 and int( number[1] ) == 0:     #입력이 0,0일때 탈출
 
         break
 
-    elif int( number[1] ) % int( number[0] ) == 0:
+    elif int( number[1] ) % int( number[0] ) == 0:      #약수일때
 
         print("factor")
 
-    elif int( number[0] ) % int( number[1] ) == 0:
+    elif int( number[0] ) % int( number[1] ) == 0:      #배수일때
 
         print("multiple")
 
-    else:
+    else:       #아무것도 아닐때
 
         print("neither")
