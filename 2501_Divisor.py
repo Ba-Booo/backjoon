fa2135469ae243a05eba4f63ebaddd6d4db8e4e8
Ba-Booo <123147457+Ba-Booo@users.noreply.github.com>
2023-05-22 22:48:14 +0900

    Add files via upload

---
+++ 2501_Divisor.py
@@ -0,0 +1,26 @@
+number = 0
+
+nk = input().split()    #약수새끼 버전시발
+
+if int( nk[0] ) % int( nk[1] ) == 0:    #k가 n의 약수일때
+
+    for i in range(1, int( nk[0] ) + 1 ):   #n만큼 반복
+
+        if int( nk[0] ) % i == 0:   #i가 약수일때
+
+            number = number + 1
+    
+
+        if number == int( nk[1] ):  #number이 k와 같을때
+
+            measure = i
+            break
+
+
+if int( nk[1] ) == number:  #k가 n의 약수 개수보다 작을때
+
+    print(measure)
+
+else:
+
+    print(0)
