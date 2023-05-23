41aee8648f9edd9997371b776e8f25b60050b1c5
Ba-Booo <123147457+Ba-Booo@users.noreply.github.com>
2023-05-23 22:08:36 +0900

    Update 2869_Snail.py

--- 2869_Snail.py
+++ 2869_Snail.py
@@ -1,11 +1,11 @@
-day = 0 #버그시발
+day = 0 
 
-snail = input().split(' ')
+snail = input().split(' ')  #입력
 
-while int( snail[2] ) > 1:
+while int( snail[2] ) > 1:  
 
-    snail[2] = int( snail[2] ) - int( snail[0] ) + int( snail[1] )
+    snail[2] = int( snail[2] ) - int( snail[0] ) + int( snail[1] )  #(거리) - (올라감) + (내려감)
     
-    day = day + 1
+    day = day + 1   #날짜
 
-print(day)
+print(day)  #출력
