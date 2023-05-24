7ac672d8866989fb31c2f5ec8b67d95f41d45ff5
Ba-Booo <123147457+Ba-Booo@users.noreply.github.com>
2023-05-24 22:49:52 +0900

    Update 2292_Honeycomb.py

--- 2292_Honeycomb.py
+++ 2292_Honeycomb.py
@@ -1,13 +1,13 @@
-distance = 0
-number = 1
+distance = 0    #거리
+number = 1  #방번호
 
-location = input()
+location = input()  #입력
 
 
-while int(location) >= number:  
+while int(location) >= number:      #(위치) >= (방번호)
 
     distance = distance + 1
     number = number + distance * 6
 
 
-print(distance + 1)
+print(distance + 1) #출력
